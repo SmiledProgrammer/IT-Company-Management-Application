f789e8aa9bdb9d8ad14959dd8fcffc341a16d0a8
Szymon <szinton@gmail.com>
2022-01-25 17:00:10 +0100

    Initial commit

---
+++ src/main/java/com/szinton/companymanager/CompanymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.szinton.companymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanymanagerApplication.class, args);
+	}
+
+}
